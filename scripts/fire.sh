08799a545208eae569e4ae112f94af43fb8fb3c8
gfw-breaker <lixiaoming0513@gmail.com>
2018-10-05 04:41:16 +0800

    同步于: 10月5日4时41分

--- scripts/fire.sh
+++ scripts/fire.sh
@@ -19,6 +19,10 @@ done
 git add ../indexes/*
 git add ../pages/*
 
+tt=$(date "+-%m%d%H%M")
+sed -i "s/\.md.*)/.md?t=$tt)/g" ../README.md
+git add ../README.md
+
 ts=$(date "+-%m月-%d日-%H时-%M分" | sed 's/-0//g' | sed 's/-//g')
 git commit -a -m "同步于: $ts"
 git push
