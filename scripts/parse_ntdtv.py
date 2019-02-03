801ce41bac7de793ff8eaf3368bbbba8da5cbb5f
gfw-breaker <lixiaoming0513@gmail.com>
2019-02-03 15:12:05 +0800

    同步于: 2月3日15时12分

--- scripts/parse_ntdtv.py
+++ scripts/parse_ntdtv.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import sys
+import os
 import requests
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
@@ -33,13 +34,12 @@ def get_name(link):
 	return aid
 
 
-def write_page(name, title, link, content):
+def write_page(path, title, link, content):
 	body = '### ' + title
 	body += "\n------------------------\n\n" + content
 	body += "\n<br/>原文链接：" + link + "\n"
 	body += "\n\n------------------------\n" + "#### [禁闻聚合首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/gfw-breaker/open-proxy/blob/master/README.md) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;|&nbsp; [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) &nbsp;|&nbsp; [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md#绪论)"
-	f_name = '../pages/' + channel + '/' +  name + '.md'
-	fh = open(f_name, 'w')
+	fh = open(path, 'w')
 	fh.write(body)
 	fh.close()
 
@@ -52,10 +52,12 @@ for article in articles:
 	a_url = link.get('href').encode('utf-8')
 	a_title = link.find('h3').text.encode('utf-8').strip()
 	name = get_name(a_url)
+	file_path = '../pages/' + channel + '/' + name + '.md'
 	content = get_content(a_url)
-	# print a_url, a_title
-	write_page(name, a_title, a_url, content)
-	index_page += '#### [' + a_title + '](' + '../pages/' + channel + '/' + name + '.md) \n\n'
+
+	if not os.path.exists(file_path):
+		write_page(file_path, a_title, a_url, content)
+	index_page += '#### [' + a_title + '](' + file_path + ') \n\n'
 
 
 index_file = open('../indexes/' + channel + '.md', 'w')
