3f31461325337343f0e6263539bb7bbafd88382d
gfw-breaker <lixiaoming0513@gmail.com>
2019-04-26 09:13:19 +0800

    同步于: 4月26日9时13分

--- scripts/macros.py
+++ scripts/macros.py
@@ -3,12 +3,12 @@
 
 git_base_url = "https://github.com/gfw-breaker/banned-news/blob/master/pages"
 
-head = "#### 精彩节目：[《文昭谈古论今》](http://134.209.198.168/wenzhao) | [《历史上的今天》](http://134.209.198.168/today-in-history) | [《天亮时分》](http://134.209.198.168/tianliang) | [《大陆新闻解读》](http://134.209.198.168/ntdtv-comedy) | [《热点互动》](http://134.209.198.168/ntdtv-rdhd)  | [《中国禁闻》](http://134.209.198.168/ntdtv-news) | [《新闻看点》](http://134.209.198.168/news-insight) \n\n "
+head = "#### 精彩节目：[《文昭谈古论今》](http://174.138.5.51/wenzhao) | [《历史上的今天》](http://174.138.5.51/today-in-history) | [《天亮时分》](http://174.138.5.51/tianliang) | [《大陆新闻解读》](http://174.138.5.51/ntdtv-comedy) | [《热点互动》](http://174.138.5.51/ntdtv-rdhd)  | [《中国禁闻》](http://174.138.5.51/ntdtv-news) | [《新闻看点》](http://174.138.5.51/news-insight) \n\n "
 
 
-head += " #### 翻墙必看： [法轮功真相](http://134.209.198.168:10000/videos/truth.html) &nbsp;&nbsp;|&nbsp;&nbsp; [活摘器官](http://134.209.198.168:10000/videos/res/Organs/) &nbsp;&nbsp;|&nbsp;&nbsp; [709维权律师大抓捕](http://134.209.198.168:10000/videos/709/) &nbsp;&nbsp;|&nbsp;&nbsp; [共产党的历史与本质](http://134.209.198.168:10000/videos/ccp.html) &nbsp;&nbsp;| [《九评共产党》](http://134.209.198.168:10000/videos/jiuping/) \n\n"
+head += " #### 翻墙必看： [法轮功真相](http://174.138.5.51:10000/videos/truth.html) &nbsp;&nbsp;|&nbsp;&nbsp; [活摘器官](http://174.138.5.51:10000/videos/res/Organs/) &nbsp;&nbsp;|&nbsp;&nbsp; [709维权律师大抓捕](http://174.138.5.51:10000/videos/709/) &nbsp;&nbsp;|&nbsp;&nbsp; [共产党的历史与本质](http://174.138.5.51:10000/videos/ccp.html) &nbsp;&nbsp;| [《九评共产党》](http://174.138.5.51:10000/videos/jiuping/) \n\n"
 
-head += "#### 反向代理： [大纪元新闻网](http://134.209.198.168:10080/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视台](http://134.209.198.168:8000/) &nbsp;&nbsp;|&nbsp;&nbsp; [希望之声](http://134.209.198.168:8200/) &nbsp;&nbsp;|&nbsp;&nbsp; [追查国际](http://134.209.198.168:10010/) &nbsp;&nbsp;|&nbsp;&nbsp; [自由亚洲电台](http://134.209.198.168:9800/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视直播](http://134.209.198.168/) \n\n" 
+head += "#### 反向代理： [大纪元新闻网](http://174.138.5.51:10080/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视台](http://174.138.5.51:8000/) &nbsp;&nbsp;|&nbsp;&nbsp; [希望之声](http://174.138.5.51:8200/) &nbsp;&nbsp;|&nbsp;&nbsp; [追查国际](http://174.138.5.51:10010/) &nbsp;&nbsp;|&nbsp;&nbsp; [自由亚洲电台](http://174.138.5.51:9800/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视直播](http://174.138.5.51/) \n\n" 
 
 tail = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/labour-camp/helloworld) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;| [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) | [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md) | [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md/blob/master/README.md)\n\n"
 
