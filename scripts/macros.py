12f7131a3d2df4384a5f0dba435b59e3d8ae4db5
gfw-breaker <lixiaoming0513@gmail.com>
2019-04-28 04:06:51 +0800

    同步于: 4月28日4时6分

--- scripts/macros.py
+++ scripts/macros.py
@@ -7,5 +7,8 @@ head = "#### [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/mast
 
 tail = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/labour-camp/helloworld) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;| [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) | [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md) | [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md/blob/master/README.md)\n\n"
 
-menu = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;&nbsp;|&nbsp;&nbsp; [精彩视频](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;&nbsp;|&nbsp;&nbsp; [大纪元新闻网](http://159.65.107.72:10080/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视台](http://159.65.107.72:8000/) &nbsp;&nbsp;|&nbsp;&nbsp; [希望之声](http://159.65.107.72:8200/) &nbsp;&nbsp;|&nbsp;&nbsp; [追查国际](http://159.65.107.72:10010/) &nbsp;&nbsp;|&nbsp;&nbsp; [自由亚洲电台](http://159.65.107.72:9800/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视直播](http://159.65.107.72/) \n\n"
+menu = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;&nbsp;|&nbsp;&nbsp; [大纪元新闻网](http://159.65.107.72:10080/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视台](http://159.65.107.72:8000/) &nbsp;&nbsp;|&nbsp;&nbsp; [希望之声](http://159.65.107.72:8200/) &nbsp;&nbsp;|&nbsp;&nbsp; [追查国际](http://159.65.107.72:10010/) &nbsp;&nbsp;|&nbsp;&nbsp; [自由亚洲电台](http://159.65.107.72:9800/) &nbsp;&nbsp;|&nbsp;&nbsp; [新唐人电视直播](http://159.65.107.72/) \n\n"
+
+menu += "#### [《天亮时分》](http://159.65.107.72/tianliang/) | [《文昭谈古论今》](http://159.65.107.72/wenzhao/) | [《历史上的今天》](http://159.65.107.72/today-in-history/) | [《大陆新闻解读》](http://159.65.107.72/ntdtv-comedy/) | [《热点互动》](http://159.65.107.72/ntdtv-rdhd/) | [《中国禁闻》](http://159.65.107.72/ntdtv-news/) | [《中国禁闻》](http://159.65.107.72/news-insight/) \n\n"
+
 
