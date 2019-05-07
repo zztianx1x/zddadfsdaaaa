0bb408b57d2813aaa8c5353d57a8381aeefdaba1
gfw-breaker <lixiaoming0513@gmail.com>
2019-05-08 03:13:53 +0800

    同步于: 5月8日3时13分

--- scripts/macros.py
+++ scripts/macros.py
@@ -5,6 +5,10 @@ git_base_url = "https://github.com/gfw-breaker/banned-news/blob/master/pages"
 
 head = "#### [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki) -  [《九评共产党》](https://github.com/gfw-breaker/9ping.md) - [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md) - [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md)\n\n" 
 
+head += "#### 网站列表： [大纪元新闻网](http://134.209.56.161:10080/) &nbsp;|&nbsp; [新唐人电视台](http://134.209.56.161:8000/) &nbsp;|&nbsp; [希望之声](http://134.209.56.161:8200/) &nbsp;|&nbsp; [追查国际](http://134.209.56.161:10010/) &nbsp;|&nbsp; [自由亚洲电台](http://134.209.56.161:9800/) &nbsp;|&nbsp; [新唐人电视直播](http://134.209.56.161/) &nbsp;|&nbsp; [维基百科](http://134.209.56.161:8100/)  \n\n"
+
+head += "#### 精彩节目： [江峰时刻](http://134.209.56.161/today-in-history/) &nbsp;|&nbsp; [天亮时分](http://134.209.56.161/tianliang/) &nbsp;|&nbsp; [文昭谈古论今](http://134.209.56.161/wenzhao/) &nbsp;|&nbsp; [大陆新闻解读](http://134.209.56.161/ntdtv-comedy/) &nbsp;|&nbsp; [热点互动](http://134.209.56.161/ntdtv-rdhd/) &nbsp;|&nbsp; [中国禁闻](http://134.209.56.161/ntdtv-news/) &nbsp;|&nbsp; [新闻看点](http://134.209.56.161/news-insight/) \n\n"
+
 tail = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/labour-camp/helloworld) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;| [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) | [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md) | [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md/blob/master/README.md)\n\n"
 
 menu = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;&nbsp;|&nbsp;&nbsp; [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki)  \n\n"
