b30986aeb92844a0d06e5b22eb4902ad413c20d0
gfw-breaker <lixiaoming0513@gmail.com>
2019-05-21 10:05:10 +0800

    同步于: 5月21日10时5分

--- scripts/macros.py
+++ scripts/macros.py
@@ -7,6 +7,10 @@ head = "#### [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki)
 
 head += "#### 由于频繁封锁，安卓用户请使用 [网门](https://github.com/oGate2/oGate) 或 [禁闻聚合](https://github.com/gfw-breaker/bn-android) 免翻墙观看热门YouTube频道 \n\n"
 
+head += "#### 网站列表： [大纪元新闻网](http://68.183.166.102:10080/) &nbsp;|&nbsp; [新唐人电视台](http://68.183.166.102:8000/) &nbsp;|&nbsp; [希望之声](http://68.183.166.102:8200/) &nbsp;|&nbsp; [追查国际](http://68.183.166.102:10010/) &nbsp;|&nbsp; [自由亚洲电台](http://68.183.166.102:9800/) &nbsp;|&nbsp; [新唐人电视直播](http://68.183.166.102/) &nbsp;|&nbsp; [维基百科](http://68.183.166.102:8100/)  \n\n"
+
+head += "#### 精彩节目： [江峰时刻](http://68.183.166.102/today-in-history/) &nbsp;|&nbsp; [天亮时分](http://68.183.166.102/tianliang/) &nbsp;|&nbsp; [老北京茶馆](http://68.183.166.102/teahouse/) &nbsp;|&nbsp; [文昭谈古论今](http://68.183.166.102/wenzhao/) &nbsp;|&nbsp; [大陆新闻解读](http://68.183.166.102/ntdtv-comedy/) &nbsp;|&nbsp; [热点互动](http://68.183.166.102/ntdtv-rdhd/) &nbsp;|&nbsp; [中国禁闻](http://68.183.166.102/ntdtv-news/) &nbsp;|&nbsp; [新闻看点](http://68.183.166.102/news-insight/) \n\n"
+
 tail = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/labour-camp/helloworld) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;| [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) | [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md) | [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md/blob/master/README.md)\n\n"
 
 menu = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;&nbsp;|&nbsp;&nbsp; [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki) &nbsp;&nbsp;|&nbsp;&nbsp; [禁闻聚合安卓版](https://github.com/gfw-breaker/bn-android) &nbsp;&nbsp;|&nbsp;&nbsp; [网门安卓版](https://git.io/ogatea2) &nbsp;&nbsp;|&nbsp;&nbsp; [神州正道安卓版](https://github.com/SzzdOgate/update) \n\n"
