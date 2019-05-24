c71387057313e9ead486fa35d6eefdf681505bfa
gfw-breaker <lixiaoming0513@gmail.com>
2019-05-24 11:32:37 +0800

    同步于: 5月24日11时32分

--- scripts/macros.py
+++ scripts/macros.py
@@ -7,6 +7,7 @@ head = "#### [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki)
 
 head += "#### 由于频繁封锁，安卓用户请使用 [网门](https://github.com/gfw-breaker/bn-android/blob/master/ogate.md) 或 [禁闻聚合](https://github.com/gfw-breaker/bn-android) 免翻墙观看热门YouTube频道 \n\n"
 
+head += "#### 明慧海外之窗：[褒奖决议](282.md) &nbsp;&nbsp;|&nbsp;&nbsp; [社会支持](140.md) &nbsp;&nbsp;|&nbsp;&nbsp; [社区报道](91.md) &nbsp;&nbsp;|&nbsp;&nbsp; [海外简讯](245.md) &nbsp;&nbsp;|&nbsp;&nbsp; [时事评论](251.md) &nbsp;&nbsp;|&nbsp;&nbsp; [迫害海外延伸](236.md) \n\n" 
 
 tail = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/labour-camp/helloworld) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;| [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) | [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md) | [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md/blob/master/README.md)\n\n"
 
