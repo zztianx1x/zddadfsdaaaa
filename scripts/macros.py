671d43479d2c124346a6ca3adaded02d2db4fdda
gfw-breaker <lixiaoming0513@gmail.com>
2019-05-28 07:32:12 +0800

    同步于: 5月28日7时32分

--- scripts/macros.py
+++ scripts/macros.py
@@ -5,9 +5,7 @@ git_base_url = "https://github.com/gfw-breaker/banned-news/blob/master/pages"
 
 head = "#### 由于频繁封锁，请参考 [手把手翻墙教程](https://github.com/gfw-breaker/guides/wiki/)，安卓用户请使用 [网门](https://github.com/gfw-breaker/bn-android/blob/master/ogate.md) 或 [禁闻聚合](https://github.com/gfw-breaker/bn-android) 免翻墙观看热门YouTube频道 \n\n"
 
-head += "#### 明慧网海外之窗：&nbsp;[社会支持](140.md) &nbsp;|&nbsp; [褒奖决议](282.md) &nbsp;|&nbsp; [社区报道](91.md) &nbsp;|&nbsp; [海外简讯](245.md) &nbsp;|&nbsp; [时事评论](251.md) &nbsp;|&nbsp; [迫害案例综述](328.md) &nbsp;|&nbsp; [中共迫害海外延伸](236.md) \n\n"
-
-head += "#### 神韵中国古典舞：&nbsp;[纽约艺术团](nf4778.md) &nbsp;|&nbsp; [国际艺术团](nf4780.md) &nbsp;|&nbsp; [世界艺术团](nf5951.md) &nbsp;|&nbsp; [巡回艺术团](nf4779.md) &nbsp;|&nbsp; [北美艺术团](nf1148019.md) &nbsp;|&nbsp; [环球艺术团](nf1299941.md)  \n\n"
+head = "#### [中国古典舞神韵特别报道](shenyun.md) &nbsp;&nbsp;|&nbsp;&nbsp; [共产党迫害法轮功实录](https://github.com/gfw-breaker/mh-news/)  \n\n"
 
 tail = "#### [首页](https://github.com/gfw-breaker/banned-news/blob/master/README.md) &nbsp;|&nbsp; [Web代理](https://github.com/labour-camp/helloworld) &nbsp;|&nbsp; [一键翻墙软件](https://github.com/gfw-breaker/nogfw/blob/master/README.md) &nbsp;| [《九评共产党》](https://github.com/gfw-breaker/9ping.md/blob/master/README.md#九评之一评共产党是什么) | [《解体党文化》](https://github.com/gfw-breaker/jtdwh.md/blob/master/README.md) | [《共产主义的终极目的》](https://github.com/gfw-breaker/gczydzjmd.md/blob/master/README.md)\n\n"
 
