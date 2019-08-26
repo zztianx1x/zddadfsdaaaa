8ea972c4605d45109e3e7a69e9253008d756ef5d
gfw-breaker <lixiaoming0513@gmail.com>
2019-08-27 02:06:38 +0800

    同步于: 8月27日2时6分

--- scripts/fire.sh
+++ scripts/fire.sh
@@ -47,8 +47,12 @@ for d in $(ls ../pages/); do
 	idx=../indexes/$d.md
 	old=../indexes/$d-earlier.md
 	echo -e "\n----\n#### [ >>> 更早内容 <<< ]($old)" >> $idx
-	lines=$(wc -l $idx)
 	echo -n > $old
+	lines=$(wc -l $idx)
+	for p in $(ls -t ../pages/$d/*.md | sed -n "$lines,250p"); do
+		title=$(head -n 1 $p | cut -c6-)
+		echo "#### [$title]($p)" >> $old
+	done
 done
 
 
